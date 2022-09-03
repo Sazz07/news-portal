f326d814742f8f5b73fcaabd408d7c6cf89db497
Sazz07 <sazzad.hossain882@gmail.com>
2022-09-04 03:09:04 +0600

    All news in a catagory and modals section done

--- js/app.js
+++ js/app.js
@@ -21,11 +21,97 @@ const displayCatagories = catagories => {
     catagories.forEach(catagory => {
         const li = document.createElement('li');
         li.classList.add('nav-item');
-        li.innerHTML = `<a class="nav-link mx-4" href="#">${catagory.category_name}</a>`;
+        li.innerHTML = `<a onclick="loadCatagoriesDetails('${catagory.category_id}')" class="nav-link mx-4" href="#">${catagory.category_name}</a>`;
         catagoriesContainer.appendChild(li);
     });
 }
 
+// All news in a Category
+
+const loadCatagoriesDetails = async category_id => {
+
+    try {
+        const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCatagoriesDetails(data.data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+const displayCatagoriesDetails = catagories => {
+
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = ``;
+
+    catagories.forEach(catagory => {
+        const makeNewsDiv = document.createElement('div');
+        makeNewsDiv.classList.add('row');
+        makeNewsDiv.innerHTML = `
+        <div class="card mb-3 d-flex flex-row">
+            <div class="col-md-4 col-sm-6">
+                <img src="${catagory.image_url}" class="img-fluid rounded-start p-3" alt="...">
+            </div>
+            <div class="col-md-8 col-sm-6">
+                <div class="card-body">
+                <h5 class="card-title">${catagory.title}</h5>
+                <p class="card-text">${catagory.details.slice(0, 500)}...</p>
+                <div class="d-flex justify-content-between">
+                <div class="d-flex">
+                  <img class="rounded-circle" style="width: 40px; height: 40px;"
+                  src="${catagory.author.img}"
+                  class="rounded-full w-10 h-10"
+                  alt="" 
+                />
+                <p class="ps-2">${catagory.author.name ? catagory.author.name : 'Author Unknown'}<br><label>${catagory.author.published_date ? catagory.author.published_date : 'Date not available'}</label></p>
+                </div>
+              <div>
+                <p><i class="fa fa-light fa-eye"></i> ${catagory.total_view ? catagory.total_view : 'No Views Yet'}</p>
+              </div>
+              <div>
+                <a onclick="loadCatagoriesModal('${catagory._id}')" data-bs-toggle="modal" data-bs-target="#catagoryDetailModal" href=""><i class="fa fa-solid fa-arrow-right"></i></a>
+              </div>
+              </div>
+                </div>
+            </div>
+        </div>
+        `;
+        newsContainer.appendChild(makeNewsDiv);
+    });
+}
+
+
+// News detail url:
+
+const loadCatagoriesModal = async news_id => {
+
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCatagoriesModal(data.data[0]);
+        // console.log((data.data[0].author));
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+const displayCatagoriesModal = catagory => {
+
+        const catagoryDetails = document.getElementById('catagory-details');
+        catagoryDetails.innerHTML = `
+            <p>Author Name: ${catagory.author.name ? catagory.author.name : 'No Author Name Found'}</p>
+            <p>Publish Date: ${catagory.author.published_date ? catagory.author.published_date : 'No Publish date found'}</p>
+            <p>Views: <i class="fa fa-light fa-eye"></i> ${catagory.total_view ? catagory.total_view : 'No Views Yet'}</p>
+            <img src="${catagory.author.img}" class="img-fluid rounded" alt="">            
+            `;
+
+}
 
 
 
